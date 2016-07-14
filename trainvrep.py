f8b19921c02e2d3a3b1db4da03d9480a133f70e6
tim9510019 <tim9510019@hotmail.com>
2016-07-14 10:31:12 +0800

    correct sarsa0, add sarsa1

--- trainvrep.py
+++ trainvrep.py
@@ -2,6 +2,7 @@ import vrep
 import numpy as np
 import sys
 import csv
+from nn import neural_net, LossHistory
 
 def Initialize():
   # just in case, close all opened connections
@@ -103,6 +104,47 @@ def ReadProximitySensor(clientID, sensor_handles):
   return  errorcode, sensor_val, sensor_state
 
 
+def sarsa1_minibatch(minibatch, model, sarsa0P):
+  """This does the heavy lifting, aka, the training. It's super jacked."""
+  X_train = []
+  y_train = []
+  # Loop through our batch and create arrays for X and y
+  # so that we can fit our model at every step.
+  for memory in minibatch:     
+    # Get stored values.
+    old_state_m, action_m, reward_m, new_state_m, new_action = memory
+    #Get the number of input
+    NUM_INPUT = len(old_state_m[0])
+    # Get prediction on old state.
+    old_qval = model.predict(old_state_m, batch_size=1)
+    # Get prediction on new state.
+    newQ = model.predict(new_state_m, batch_size=1)
+    # Get our best move. (Sarsa 0 / Q-Learning)
+    updateQ = newQ[new_action]
+    #Get the number of output
+    NUM_OUTPUT = len(newQ[0])
+
+    y = np.zeros((1, NUM_OUTPUT))
+    y[:] = old_qval[:]
+        
+    # Check for terminal state.
+    if reward_m != sarsa0P[0]:  # non-terminal state
+      update = (reward_m + (sarsa0P[1] * updateQ))
+    else:  # terminal state
+      update = reward_m
+
+    # Update the value for the action we took.
+    y[0][action_m] = update
+        
+    X_train.append(old_state_m.reshape(NUM_INPUT,))
+    y_train.append(y.reshape(NUM_OUTPUT,))
+
+  X_train = np.array(X_train)
+  y_train = np.array(y_train)
+
+  return X_train, y_train
+  
+ 
 def sarsa0_minibatch(minibatch, model, sarsa0P):
   """This does the heavy lifting, aka, the training. It's super jacked."""
   X_train = []
@@ -135,13 +177,24 @@ def sarsa0_minibatch(minibatch, model, sarsa0P):
     # Update the value for the action we took.
     y[0][action_m] = update
         
-    X_train.append(new_state_m.reshape(NUM_INPUT,))
+    X_train.append(old_state_m.reshape(NUM_INPUT,))
     y_train.append(y.reshape(NUM_OUTPUT,))
 
   X_train = np.array(X_train)
   y_train = np.array(y_train)
 
   return X_train, y_train
+
+
+def train_models(X_train, y_train, batchSize, model, loss_log):
+  history = LossHistory()
+  model.fit(
+    X_train, y_train, batch_size=batchSize,
+    nb_epoch=1, verbose=0, callbacks=[history]
+  )
+  loss_log.append(history.losses)
+
+  return loss_log
 
 
 def save_models(filename, model, trainCount, interval):
